4f1abdfed68f2c11f26a433e8dced634ca1029bc
ArkhamDm <114360233+ArkhamDm@users.noreply.github.com>
2023-01-20 17:43:33 +0300

    Add files via upload

---
+++ API/src/data/Client.java
@@ -0,0 +1,41 @@
+package data;
+
+import java.io.Serializable;
+
+public class Client implements Serializable {
+    private String login;
+    private String password;
+    private String email;
+    private boolean is_admin = false;
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isIs_admin() {
+        return is_admin;
+    }
+
+    public void setIs_admin(boolean is_admin) {
+        this.is_admin = is_admin;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
