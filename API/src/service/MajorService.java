d75104a68f14872514dc260c6b4b82d6eca0edfb
ArkhamDm <114360233+ArkhamDm@users.noreply.github.com>
2023-01-20 17:45:24 +0300

    change of MajorServices

--- API/src/service/MajorService.java
+++ API/src/service/MajorService.java
@@ -1,5 +1,6 @@
 package service;
 
+import data.Client;
 import data.Deadline;
 import data.Major;
 
@@ -8,10 +9,14 @@ import java.util.ArrayList;
 public interface MajorService {
     String addMajor(Major major);
     String addDeadline(Deadline deadline, String id);
+    void addClient(Client client);
     void delMajor(String id);
     void delDeadline(String id);
     void changeMajor(Major major);
     void changeDeadline(Deadline deadline);
     ArrayList<Deadline> getDeadlines(String id);
     ArrayList<Major> getMajors();
+    ArrayList<Client> getClients();
+    Client getClient(String login, String password);
+
 }
